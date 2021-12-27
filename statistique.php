3bc850d0bf19ce64754845ae03d8465bbf0a7e95
TommyIUT <85343552+TommyIUT@users.noreply.github.com>
2021-12-27 22:52:08 +0100

    Tableau des statistiques patients

--- statistique.php
+++ statistique.php
@@ -3,7 +3,21 @@
     if (!isset($_SESSION['login'])) {
         header('Location: login.php');
     }
+
+
+    ///Connexion au serveur MySQL
+    try {
+        $linkpdo = new PDO("mysql:host=localhost;dbname=cabinet_medical", 'root'); 
+    }catch (PDOException $e){
+        die('Erreur : ' . $e->getMessage());
+    }
     
+    ///Verification de la connexion 
+    if (mysqli_connect_errno()) { 
+        print("Connect failed: \n" . mysqli_connect_error()); 
+        exit(); 
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -40,6 +54,8 @@
 
         <!-- Corps de la page -->
         <section>
+
+        <!-- Tableau des nombres d'hommes et femmes en fonction de leur age -->
         <table>
                 <thead>
                         <tr>
@@ -50,18 +66,74 @@
                 </thead>
                     <tr>
                     <th>Moins de 25 ans</th>
-                        <th> oui </th>
-                        <th> non </th>
+                        <th> 
+                            <?php
+		                    	$reqHjeune = $linkpdo->query("SELECT COUNT(*) as Hjeune 
+                                                        FROM patient 
+                                                        WHERE DATEDIFF(NOW(), DateNaissance) < 25*365.25 
+                                                        AND CiviliteP = 'M'");
+                                $nbHjeune=$reqHjeune->fetchColumn(); 
+                                echo $nbHjeune;
+                            ?>
+                        </th>
+                        <th>
+                        <?php
+		                    	$reqFjeune = $linkpdo->query("SELECT COUNT(*) as Fjeune 
+                                                        FROM patient 
+                                                        WHERE DATEDIFF(NOW(), DateNaissance) < 25*365.25 
+                                                        AND (CiviliteP = 'Mme' OR CiviliteP = 'Mlle')");
+                                $nbFjeune=$reqFjeune->fetchColumn(); 
+                                echo $nbFjeune;
+                            ?>
+                        </th>
                     </tr>
                     <tr>
                     <th>Entre 25 et 50 ans</th>
-                        <th> oui </th>
-                        <th> non </th>
+                    <th> 
+                            <?php
+		                    	$reqHmoyen = $linkpdo->query("SELECT COUNT(*) as Hmoyen
+                                                        FROM patient 
+                                                        WHERE DATEDIFF(NOW(), DateNaissance) > 25*365.25 
+                                                        AND DATEDIFF(NOW(), DateNaissance) < 50*365.25
+                                                        AND CiviliteP = 'M'");
+                                $nbHmoyen=$reqHmoyen->fetchColumn(); 
+                                echo $nbHmoyen;
+                            ?>
+                        </th>
+                        <th>
+                        <?php
+		                    	$reqFmoyenne = $linkpdo->query("SELECT COUNT(*) as Fmoyenne 
+                                                        FROM patient 
+                                                        WHERE DATEDIFF(NOW(), DateNaissance) > 25*365.25 
+                                                        AND DATEDIFF(NOW(), DateNaissance) < 50*365.25
+                                                        AND (CiviliteP = 'Mme' OR CiviliteP = 'Mlle')");
+                                $nbFmoyenne=$reqFmoyenne->fetchColumn(); 
+                                echo $nbFmoyenne;
+                            ?>
+                        </th>
                     </tr>
                     <tr>
                     <th>Plus de 50 ans</th>
-                        <th> oui </th>
-                        <th> non </th>
+                    <th> 
+                            <?php
+		                    	$reqHvieux = $linkpdo->query("SELECT COUNT(*) as Hvieux
+                                                        FROM patient 
+                                                        WHERE DATEDIFF(NOW(), DateNaissance) > 50*365.25 
+                                                        AND CiviliteP = 'M'");
+                                $nbHvieux=$reqHvieux->fetchColumn(); 
+                                echo $nbHvieux;
+                            ?>
+                        </th>
+                        <th>
+                        <?php
+		                    	$reqFvieille = $linkpdo->query("SELECT COUNT(*) as Fvieille
+                                                        FROM patient 
+                                                        WHERE DATEDIFF(NOW(), DateNaissance) > 50*365.25 
+                                                        AND (CiviliteP = 'Mme' OR CiviliteP = 'Mlle')");
+                                $nbFvieille=$reqFvieille->fetchColumn(); 
+                                echo $nbFvieille;
+                            ?>
+                        </th>
                     </tr>
         </table>
 
