09c59b9fdd0665bf218e57e14098c473b4c4de9d
TommyIUT <85343552+TommyIUT@users.noreply.github.com>
2022-01-05 11:02:39 +0100

    Update ajoutConsultation.php

--- ajoutConsultation.php
+++ ajoutConsultation.php
@@ -2,26 +2,45 @@
 require 'connexionPDO.php';
 
 //Conversion de la date et heure en entier
+$stringDate
+
+//Récupération id Patient
+$reqIdP = $linkpdo->prepare("SELECT *
+                            FROM patient
+                            WHERE NomP = :nomP
+                            AND PrenomP = :prenomP ");
+$reqIdP -> bindParam(':nomP', $_POST['NomP']);
+$reqIdP -> bindParam(':prenomP', $_POST['PrenomP']);
+$reqIdP -> execute();
+
+$data = $reqIdP->fetch();
+$idP = $data['Id_Patient'];
+
+//Récupération id Médecin
+$reqIdM = $linkpdo->prepare("SELECT *
+                            FROM medecin
+                            WHERE Nom = :nomM");
+$reqIdM -> bindParam(':nomM', $_POST['NomM']);
+$reqIdM -> execute();
+
+$data = $reqIdM->fetch();
+$idM = $data['Id_Medecin'];
+
+//Conversion de la durée en entier
+$nbh = $_POST['HeureD'];
+$nbmin = $_POST['MinutesD'];
+$dureeInt = 60*$nbmin + 3600*$nbh;
 
 
+//$req = $linkpdo->prepare('INSERT INTO consultation(DateEtHeureConsultation, DureeConsultation, Id_Medecin, Id_Patient)
+//VALUES (:dateHeure, :duree, :idM, :idP)');
 
+//$req -> bindParam(':civilite', $_POST['civilite']);
+//$req -> bindParam(':nom', $_POST['nom']);
+//$req -> bindParam(':prenom', $_POST['prenom']);
+//$req->execute();
 
-$req = $linkpdo->prepare('INSERT INTO patient(CiviliteP, NomP,PrenomP,Adresse,CodePostal,Ville,DateNaissance,LieuNaissance,Numero)
-VALUES (:civilite, :nom, :prenom, :adresse, :cp, :ville, :datenaiss, :lieunaiss, :secu)');
-
-$req -> bindParam(':civilite', $_POST['civilite']);
-$req -> bindParam(':nom', $_POST['nom']);
-$req -> bindParam(':prenom', $_POST['prenom']);
-$req -> bindParam(':adresse', $_POST['adresse']);
-$req -> bindParam(':cp', $_POST['cp']);
-$req -> bindParam(':ville', $_POST['ville']);
-$req -> bindParam(':datenaiss', $_POST['datenaiss']);
-$req -> bindParam(':lieunaiss', $_POST['lieunaiss']);
-$req -> bindParam(':secu', $_POST['secu']);
-
-$req->execute();
-
-header('Location: ./usager.php');
+//header('Location: ./consultation.php');
 
 
 ?>
